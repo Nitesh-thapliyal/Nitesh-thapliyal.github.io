7b3b7de240af52afaa02715b48833b5af2976e6f
Nitesh Thapliyal <niteshnits.thapliyal@gmail.com>
2022-08-15 11:34:30 +0530

    update

--- script.js
+++ script.js
@@ -14,3 +14,66 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+
+const blogData = [{
+    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--EPAmrzdD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2pfqg9722bu92rfwpu6l.png',
+    heading: 'Run GUI Applicaiton inside Docker Container',
+    para: 'Ever wonder how you can run GUI application inside a container?',
+    link: '#'
+},
+{
+    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--EPAmrzdD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2pfqg9722bu92rfwpu6l.png',
+    heading: 'Run GUI Applicaiton inside Docker Container',
+    para: 'Ever wonder how you can run GUI application inside a container?',
+    link: '#'
+},
+
+{
+    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--EPAmrzdD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2pfqg9722bu92rfwpu6l.png',
+    alt: 'image',
+    heading: 'Run GUI Applicaiton inside Docker Container',
+    para: 'Ever wonder how you can run GUI application inside a container?',
+    link: '#'
+},
+{
+    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--EPAmrzdD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2pfqg9722bu92rfwpu6l.png',
+    alt: 'image',
+    heading: 'Run GUI Applicaiton inside Docker Container',
+    para: 'Ever wonder how you can run GUI application inside a container?',
+    link: '#'
+},
+{
+    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--EPAmrzdD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2pfqg9722bu92rfwpu6l.png',
+    alt: 'image',
+    heading: 'Run GUI Applicaiton inside Docker Container',
+    para: 'Ever wonder how you can run GUI application inside a container?',
+    link: '#'
+},
+{
+    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--EPAmrzdD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2pfqg9722bu92rfwpu6l.png',
+    alt: 'image',
+    heading: 'Run GUI Applicaiton inside Docker Container',
+    para: 'Ever wonder how you can run GUI application inside a container?',
+    link: '#'
+}
+]
+
+
+const blogSection = document.querySelector('.blog')
+
+blogData.forEach(blogInsert)
+
+function blogInsert(data) {
+    let blogData = `<div class="blog-box">
+    <div>
+      <div class="blog-image"><img src ="${data.image}"alt="${data.alt}"></div>
+      <div class="blog-content">
+        <h1>${data.heading}</h1>
+        <p>${data.para}</p>
+      </div>
+    </div>
+    <div class="blog-button"><a href="${data.link}">Read More</a></div>
+  </div>`
+    blogSection.innerHTML += blogData
+}
