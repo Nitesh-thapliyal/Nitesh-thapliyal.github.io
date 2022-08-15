4141cb989a41680e6a9a53908d20230ce424420d
Nitesh Thapliyal <niteshnits.thapliyal@gmail.com>
2022-08-15 12:49:44 +0530

    15aug_update

--- script.js
+++ script.js
@@ -17,45 +17,47 @@ productContainers.forEach((item, i) => {
 
 
 const blogData = [{
-    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--EPAmrzdD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2pfqg9722bu92rfwpu6l.png',
+    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--XkS2pez7--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/q00qx8y8vihfhw3ng6fa.png',
+    alt: 'image',
     heading: 'Run GUI Applicaiton inside Docker Container',
     para: 'Ever wonder how you can run GUI application inside a container?',
-    link: '#'
+    link: 'https://dev.to/niteshthapliyal/run-gui-application-inside-docker-container-3npg'
 },
 {
-    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--EPAmrzdD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2pfqg9722bu92rfwpu6l.png',
-    heading: 'Run GUI Applicaiton inside Docker Container',
-    para: 'Ever wonder how you can run GUI application inside a container?',
-    link: '#'
+    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--KOszsDAN--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/m3djekhpg8svn8ubklv9.png',
+    alt: 'image',
+    heading: 'Configuring Kubernetes Multi-Node Cluster using Ansible over AWS',
+    para: 'In this blog, we will discuss how we can create Kubernetes multi-node cluster using Ansible role over Amazon Web Service (AWS)',
+    link: 'https://dev.to/niteshthapliyal/configuring-kubernetes-multi-node-cluster-using-ansible-over-aws-2cki'
 },
 
 {
-    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--EPAmrzdD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2pfqg9722bu92rfwpu6l.png',
+    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--xUPvATkn--/c_imagga_scale,f_auto,fl_progressive,h_420,q_66,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/1cugelnze8gv6z2w39iy.gif',
     alt: 'image',
-    heading: 'Run GUI Applicaiton inside Docker Container',
-    para: 'Ever wonder how you can run GUI application inside a container?',
-    link: '#'
+    heading: 'WordPress configuration over AWS with Amazon RDS',
+    para: 'In this blog, we will be configuring WordPress over AWS and will use AWS RDS as a backend for WordPress',
+    link: 'https://dev.to/niteshthapliyal/wordpress-configuration-over-aws-with-amazon-rds-j76'
 },
 {
-    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--EPAmrzdD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2pfqg9722bu92rfwpu6l.png',
+    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--ccmSR4T5--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/zn0pmeo9yseuoohph380.png',
     alt: 'image',
-    heading: 'Run GUI Applicaiton inside Docker Container',
-    para: 'Ever wonder how you can run GUI application inside a container?',
-    link: '#'
+    heading: 'Hadoop Configuration using Ansible',
+    para: 'This blog contains the configuration or setup of the Hadoop cluster using Ansible',
+    link: 'https://dev.to/niteshthapliyal/hadoop-configuration-using-ansible-3ena'
 },
 {
-    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--EPAmrzdD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2pfqg9722bu92rfwpu6l.png',
+    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--TCD5yqG3--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/8vu1litee2wa3zy5ese6.png',
     alt: 'image',
-    heading: 'Run GUI Applicaiton inside Docker Container',
-    para: 'Ever wonder how you can run GUI application inside a container?',
-    link: '#'
+    heading: 'Setup that can ping Google but not Facebook',
+    para: 'In this blog we are going to configure the routing table in such a way that it can ping Google but not Facebook',
+    link: 'https://dev.to/niteshthapliyal/setup-that-can-ping-google-but-not-facebook-1ea4'
 },
 {
-    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--EPAmrzdD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2pfqg9722bu92rfwpu6l.png',
+    image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--q8ddEfOx--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/61s9aiuzz7fxom9kyzau.png',
     alt: 'image',
-    heading: 'Run GUI Applicaiton inside Docker Container',
-    para: 'Ever wonder how you can run GUI application inside a container?',
-    link: '#'
+    heading: 'Create ML model inside the docker container',
+    para: 'This blog contains the demonstration of how we can create a Machine learning model inside the docker container',
+    link: 'https://dev.to/niteshthapliyal/create-ml-model-inside-the-docker-container-3b23'
 }
 ]
 
